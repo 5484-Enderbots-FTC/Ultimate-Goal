bdbf48a978dfff93e9fcc8f69c16c6d6dbcdfdc0
yaomat <mattruss2014@gmail.com>
2020-10-17 12:40:06 -0400

    yes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDeterminationExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDeterminationExample.java
@@ -93,11 +93,10 @@ public class SkystoneDeterminationExample extends LinearOpMode
         /*
          * An enum to define the skystone position
          */
-        public enum SkystonePosition
+        public enum RingLocation
         {
-            LEFT,
-            CENTER,
-            RIGHT
+            THERE,
+            NOT_THERE
         }
 
         /*
@@ -110,8 +109,6 @@ public class SkystoneDeterminationExample extends LinearOpMode
          * The core values which define the location and size of the sample regions
          */
         static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(109,98);
-        static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(181,98);
-        static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(253,98);
         static final int REGION_WIDTH = 20;
         static final int REGION_HEIGHT = 20;
 
@@ -138,18 +135,7 @@ public class SkystoneDeterminationExample extends LinearOpMode
         Point region1_pointB = new Point(
                 REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
                 REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-        Point region2_pointA = new Point(
-                REGION2_TOPLEFT_ANCHOR_POINT.x,
-                REGION2_TOPLEFT_ANCHOR_POINT.y);
-        Point region2_pointB = new Point(
-                REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-        Point region3_pointA = new Point(
-                REGION3_TOPLEFT_ANCHOR_POINT.x,
-                REGION3_TOPLEFT_ANCHOR_POINT.y);
-        Point region3_pointB = new Point(
-                REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
 
         /*
          * Working variables
@@ -160,7 +146,7 @@ public class SkystoneDeterminationExample extends LinearOpMode
         int avg1, avg2, avg3;
 
         // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile SkystonePosition position = SkystonePosition.LEFT;
+        private volatile RingLocation position = RingLocation.THERE;
 
         /*
          * This function takes the RGB frame, converts to YCrCb,
@@ -192,8 +178,6 @@ public class SkystoneDeterminationExample extends LinearOpMode
              * reverse also holds true.
              */
             region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
-            region2_Cb = Cb.submat(new Rect(region2_pointA, region2_pointB));
-            region3_Cb = Cb.submat(new Rect(region3_pointA, region3_pointB));
         }
 
         @Override
@@ -247,8 +231,6 @@ public class SkystoneDeterminationExample extends LinearOpMode
              * at index 2 here.
              */
             avg1 = (int) Core.mean(region1_Cb).val[0];
-            avg2 = (int) Core.mean(region2_Cb).val[0];
-            avg3 = (int) Core.mean(region3_Cb).val[0];
 
             /*
              * Draw a rectangle showing sample region 1 on the screen.
@@ -265,38 +247,24 @@ public class SkystoneDeterminationExample extends LinearOpMode
              * Draw a rectangle showing sample region 2 on the screen.
              * Simply a visual aid. Serves no functional purpose.
              */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region2_pointA, // First point which defines the rectangle
-                    region2_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
 
             /*
              * Draw a rectangle showing sample region 3 on the screen.
              * Simply a visual aid. Serves no functional purpose.
              */
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    region3_pointA, // First point which defines the rectangle
-                    region3_pointB, // Second point which defines the rectangle
-                    BLUE, // The color the rectangle is drawn in
-                    2); // Thickness of the rectangle lines
 
 
             /*
              * Find the max of the 3 averages
              */
-            int maxOneTwo = Math.max(avg1, avg2);
-            int max = Math.max(maxOneTwo, avg3);
 
             /*
              * Now that we found the max, we actually need to go and
              * figure out which sample region that value was from
              */
-            if(max == avg1) // Was it from region 1?
+            if(avg1 <=115) // Was it from region 1?
             {
-                position = SkystonePosition.LEFT; // Record our analysis
+                position = RingLocation.THERE; // Record our analysis
 
                 /*
                  * Draw a solid rectangle on top of the chosen region.
@@ -309,36 +277,7 @@ public class SkystoneDeterminationExample extends LinearOpMode
                         GREEN, // The color the rectangle is drawn in
                         -1); // Negative thickness means solid fill
             }
-            else if(max == avg2) // Was it from region 2?
-            {
-                position = SkystonePosition.CENTER; // Record our analysis
 
-                /*
-                 * Draw a solid rectangle on top of the chosen region.
-                 * Simply a visual aid. Serves no functional purpose.
-                 */
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        region2_pointA, // First point which defines the rectangle
-                        region2_pointB, // Second point which defines the rectangle
-                        GREEN, // The color the rectangle is drawn in
-                        -1); // Negative thickness means solid fill
-            }
-            else if(max == avg3) // Was it from region 3?
-            {
-                position = SkystonePosition.RIGHT; // Record our analysis
-
-                /*
-                 * Draw a solid rectangle on top of the chosen region.
-                 * Simply a visual aid. Serves no functional purpose.
-                 */
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        region3_pointA, // First point which defines the rectangle
-                        region3_pointB, // Second point which defines the rectangle
-                        GREEN, // The color the rectangle is drawn in
-                        -1); // Negative thickness means solid fill
-            }
 
             /*
              * Render the 'input' buffer to the viewport. But note this is not
@@ -351,7 +290,7 @@ public class SkystoneDeterminationExample extends LinearOpMode
         /*
          * Call this from the OpMode thread to obtain the latest analysis
          */
-        public SkystonePosition getAnalysis()
+        public RingLocation getAnalysis()
         {
             return position;
         }
