8d6e9b67acbccf963f52d1eeee835602e0a4cbe4
Madeline Ortiz <ortiz97mi@gmail.com>
2021-01-24 15:07:37 -0500

    opencv w webcam ACTUALLY WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV_test.java
@@ -32,100 +32,47 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
-@TeleOp
-public class EasyOpenCVExample extends LinearOpMode{
-OpenCvCamera webcam;
-SkystoneDeterminationPipeline pipeline;
+@TeleOp(name = "openCV test", group = "Concept")
+public class OpenCV_test extends LinearOpMode {
+    OpenCvCamera webcam;
+    SkystoneDeterminationPipeline pipeline;
 
-@Override
-public void runOpMode()
-        {
+    @Override
+    public void runOpMode()
+    {
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
         pipeline = new SkystoneDeterminationPipeline();
+
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName);
         webcam.setPipeline(new SkystoneDeterminationPipeline());
+        webcam.openCameraDeviceAsync(() -> {
 
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-@Override
-public void onOpened()
-        {
-
-        webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-        }
         });
+
 
         telemetry.addLine("Waiting for start");
         telemetry.update();
-
         waitForStart();
 
         while (opModeIsActive())
         {
-        /*
-         * Send some stats to the telemetry
-         */
+            telemetry.addLine("In opMode");
             telemetry.addData("Analysis", pipeline.getAnalysis());
             telemetry.addData("Position", pipeline.position);
-        telemetry.addData("Frame Count", webcam.getFrameCount());
-        telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
-        telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
-        telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
-        telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
-        telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
-        telemetry.update();
+            telemetry.update();
 
-        /*
-         * NOTE: stopping the stream from the camera early (before the end of the OpMode
-         * when it will be automatically stopped for you) *IS* supported. The "if" statement
-         * below will stop streaming from the camera when the "A" button on gamepad 1 is pressed.
-         */
-        if(gamepad1.a)
-        {
-        /*
-         * IMPORTANT NOTE: calling stopStreaming() will indeed stop the stream of images
-         * from the camera (and, by extension, stop calling your vision pipeline). HOWEVER,
-         * if the reason you wish to stop the stream early is to switch use of the camera
-         * over to, say, Vuforia or TFOD, you will also need to call closeCameraDevice()
-         * (commented out below), because according to the Android Camera API documentation:
-         *         "Your application should only have one Camera object active at a time for
-         *          a particular hardware camera."
-         *
-         * NB: calling closeCameraDevice() will internally call stopStreaming() if applicable,
-         * but it doesn't hurt to call it anyway, if for no other reason than clarity.
-         *
-         * NB2: if you are stopping the camera stream to simply save some processing power
-         * (or battery power) for a short while when you do not need your vision pipeline,
-         * it is recommended to NOT call closeCameraDevice() as you will then need to re-open
-         * it the next time you wish to activate your vision pipeline, which can take a bit of
-         * time. Of course, this comment is irrelevant in light of the use case described in
-         * the above "important note".
-         */
-        webcam.stopStreaming();
-        //webcam.closeCameraDevice();
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(50);
         }
+    }
 
-        /*
-         * For the purposes of this sample, throttle ourselves to 10Hz loop to avoid burning
-         * excess CPU cycles for no reason. (By default, telemetry is only sent to the DS at 4Hz
-         * anyway). Of course in a real OpMode you will likely not want to do this.
-         */
-        sleep(100);
-        }
-        }
+    public static class SkystoneDeterminationPipeline extends OpenCvPipeline {
 
-
-    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
-    {
-        /*
-         * An enum to define the skystone position
-         */
         public enum RingPosition
         {
             FOUR,
@@ -136,8 +83,9 @@ public void onOpened()
         /*
          * Some color constants
          */
-        static final Scalar BLUE = new Scalar(0, 0, 255, 255);
-        static final Scalar GREEN = new Scalar(0, 255, 0, 255);
+        static final Scalar BLUE = new Scalar(0, 0, 255);
+        static final Scalar GREEN = new Scalar(0, 255, 0);
+
         /*
          * The core values which define the location and size of the sample regions
          */
